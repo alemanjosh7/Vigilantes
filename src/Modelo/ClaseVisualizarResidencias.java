f59504f7e7cf62f441397fcc56ecbe59c75b5ca3
Carlos39852 <84060946+Carlos39852@users.noreply.github.com>
2021-08-12 21:17:19 -0600

    Teminando Clase de Vizualizar Residencias

---
+++ src/Modelo/ClaseVisualizarResidencias.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+/**
+ *
+ * @author ALFARO
+ */
+public class ClaseVisualizarResidencias {
+    public Connection conectar(){
+        
+     Connection cn = null;
+     try{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        //*pedira importar import java.sql.DriverManager;*/
+        cn = DriverManager.getConnection("jdbc:sqlserver: DESKTOP-QJCLPUN\\SQLEXPRESS; databaseName=TBDS_Sistema_Vigilante; IntegratedSecurity=true;");
+        //"jdbc:sqlserver://LAPTOP-7QMN6CMR\\SQLEXPRESS
+        if(cn!=null){
+          System.out.println("se conecta");
+        }
+      }
+      catch(Exception ex){
+         System.out.println(ex.getMessage());
+      }
+        return cn;
+    }
+}
