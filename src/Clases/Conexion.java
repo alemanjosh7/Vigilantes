7982073a233b531588fb6d9465cdefbfd0966527
CRISTIAN <CRISTIAN@LAPTOP-8K90LO2P>
2021-08-27 07:26:43 -0700

    Conexion actualizada

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -15,21 +15,42 @@ import java.sql.DriverManager;
  * Segundo desarrollo de software
  */
 public class Conexion {
+    private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private String url = "jdbc:sqlserver:2.//tcp.ngrok.io:18658; databaseName=TBDS_Sistema_Vigilantes";
+    private String user = "PTC_NewLogin";
+    private String pswd = "ITR2021";
     public Connection cn = null;
 
+    
+//    public Connection conectar(){
+//     try{
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        //*pedira importar import java.sql.DriverManager;*/
+//        cn = DriverManager.getConnection("jdbc:sqlserver://LAPTOP-8K90LO2P\\SQLSERVER; databaseName=TBDS_Sistema_Vigilantes; IntegratedSecurity=true;");
+//        //"jdbc:sqlserver://LAPTOP-7QMN6CMR\\SQLEXPRESS
+//        if(cn!=null){
+//            System.out.println("se conecta");
+//        }
+//      }
+//      catch(Exception ex){
+//         System.out.println(ex.getMessage());
+//      }
+//        return cn;
+//    }
+    
     public Connection conectar(){
-     try{
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        //*pedira importar import java.sql.DriverManager;*/
-        cn = DriverManager.getConnection("jdbc:sqlserver://LAPTOP-8K90LO2P\\SQLSERVER; databaseName=TBDS_Sistema_Vigilantes; IntegratedSecurity=true;");
-        //"jdbc:sqlserver://LAPTOP-7QMN6CMR\\SQLEXPRESS
-        if(cn!=null){
-            System.out.println("se conecta");
+        try{
+            Class.forName(driver);
+            //Importar el driver
+            cn = DriverManager.getConnection(url,user,pswd);
+            if (cn !=null) {
+                System.out.println("Conexión exitosa");
+            }
+            
         }
-      }
-      catch(Exception ex){
-         System.out.println(ex.getMessage());
-      }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
         return cn;
     }
 }
